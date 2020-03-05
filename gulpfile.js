6c5f67d7efb55f5aecf15d488be24a086dd262aa
User <astakhin.rsm@mail.ru>
2020-03-05 19:22:49 +0300

    Added common settings

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp'),
+    less = require('gulp-less'),
+    prefixer = require('gulp-autoprefixer'),
+    clean = require('gulp-clean-css');
+
+gulp.task('compile-css', function () {
+    return gulp.src('./assets/css/sources/main.less')
+        .pipe(less())
+        .pipe(prefixer({
+            overrideBrowserslist: ["last 5 versions"]
+        }))
+        .pipe(clean())
+        .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./assets/css/sources/**/*.less', gulp.series('compile-css'));
+});
+
+gulp.task('default', gulp.series('compile-css', 'watch'));
