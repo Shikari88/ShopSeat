704fec2f5a1e62bdaa4311c1bccda17ddd36a423
User <astakhin.rsm@mail.ru>
2020-06-06 10:27:19 +0300

    пишу код для полей ввода и кнопок у форм

---
+++ assets/js/public/es6/form.js
@@ -0,0 +1,11 @@
+'use strict';
+window.onload = function () {
+    let control = document.getElementsByClassName();
+    let btn = document.getElementById("btn");
+
+    btn.onclick = function () {
+        //if (control.value = "") {
+            alert("Введите номер");
+        //}
+    };
+};
